0f7fe17895cb81f15ea85aad1defd78584c288ec
Denis Volk <the.denis.volk@gmail.com>
2023-04-11 16:01:40 +0400

    Update aux files

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
-    version='0.1.0',
-    description='Model for detecting attacks in Ethereum smart contracts',
+    version='0.1.1',
+    description='OkGPT, a Telegram bot that uses OpenAI\'s GPT-4 to answer questions',
     author='Denis Volk <denis.volk@toptal.com>',
-    license='Private',
+    license='Public',
 )
